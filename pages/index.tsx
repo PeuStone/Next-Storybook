c79a585508133c42d68e553c34c913a4513bd20a
PeuStone <pedrohmotaaa@gmail.com>
2024-03-21 10:11:13 -0300

    Adicionando o styled components e personalizando o Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Button from '../src/components/Button/Button';
+import ThemeProvider from '../src/theme/ThemeProvider';
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Page!</h1>
-      <Button>
-        Botão simples
-      </Button>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Home Page!</h1>
+        <Button variant='primary'>
+          Botão simples
+        </Button>
+      </div>
+    </ThemeProvider>
   )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,29 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledButton = styled.button<ButtonProps> `
+  cursor: pointer;
+  border: 0;
+  padding: 8px 14px;
+  transition: 0.3s;
+  ${({ theme, variant }) => {
+    console.log(theme);    
+
+    return {
+      backgroundColor: theme.colors[variant].main,
+      color: theme.colors[variant].text,
+      ':hover': {
+        backgroundColor: theme.colors[variant].light,
+      },
+      ':focus': {
+        backgroundColor: theme.colors[variant].dark,
+      }
+    };
+  }}
+`;
 
 interface ButtonProps {
+  variant?: 'primary'| 'accent';
   children: React.ReactNode
   disabled?: boolean
   onClick?: React.MouseEventHandler<HTMLButtonElement>
@@ -8,12 +31,13 @@ interface ButtonProps {
 
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button {...props}>
+    <StyledButton {...props}>
       {children}
-    </button>
+    </StyledButton>
   )
 }
 
 Button.defaultProps = {
-  disabled: false
+  disabled: false,
+  variant: 'primary'
 }
