cc6f75b1ddaa54026fe25553d5bb587af86a1088
PeuStone <pedrohmotaaa@gmail.com>
2024-03-21 09:37:38 -0300

    Criação do componente Text e de seu respectivo Storybook

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Text from "./Text";
+
+const meta: Meta = {
+  title: 'Components/Text',
+  component: Text
+}
+export default meta;
+
+type Story = StoryObj<typeof Text>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Texto para ser modificado'
+  }
+}
