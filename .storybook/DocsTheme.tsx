089df0291d8352a149be126bfd049c493969731b
PeuStone <pedrohmotaaa@gmail.com>
2024-03-25 09:34:13 -0300

    Customizando o Theme do StoryBook

---
+++ .storybook/DocsTheme.tsx
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Nome da empresa',
+  brandUrl: 'www.alura.com',
+  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self'
+})
---
+++ .storybook/manager.tsx
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import docTheme from './DocsTheme';
+
+addons.setConfig({
+  theme: docTheme,
+})
